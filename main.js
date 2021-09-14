c4cff6e7b4a678c2b247833ad47b617cf42a78a0
mukit hasan pranto <mukithasanpranto4@gmail.com>
2021-09-14 16:09:47 +0600

    digital_clock

---
+++ main.js
@@ -0,0 +1,41 @@
+// digital clock
+
+setInterval(showTime,1000);
+
+function showTime(){
+    let current_time = new Date();
+    let hr = current_time.getHours();
+    let min = current_time.getMinutes();
+    let sec = current_time.getSeconds();
+    let am_pm;
+    let msg;
+
+    if(hr >= 12){
+        am_pm = "PM";
+    }else{
+        am_pm = "AM";
+    }
+
+    if(hr >= 6 && hr <12){
+        msg = "Good Morning!";
+    }else if(hr >= 12 && hr < 16){
+        msg = "Good Noon!"
+    }else if(hr >= 16 && hr < 18){
+        msg = "Good Afternoon!";
+    }else if(hr >= 18 && hr < 24){
+        msg = "Good Evening!";
+    }else{
+        msg = "Good Night!";
+    }
+
+    document.getElementById('clock_msg').innerHTML = msg;
+
+    hr = hr > 12? hr-12:hr;
+    hr = hr < 10 ? "0" + hr : hr;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let clock_time = hr+":"+min+":"+sec+" "+am_pm+"<br>"+current_time.toDateString();
+    
+    document.getElementById('clock').innerHTML = clock_time;
+}
